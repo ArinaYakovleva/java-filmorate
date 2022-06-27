83e8235a89b72e65b2038e8703f9e939e31c08ab
Arina <yakovlevaarischa@yandex.ru>
2022-06-28 02:13:54 +0300

    feat: logging added

--- src/main/java/application/controllers/FilmController.java
+++ src/main/java/application/controllers/FilmController.java
@@ -1,6 +1,7 @@
 package application.controllers;
 
 import application.model.Film;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +13,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
     private final Map<Integer, Film> filmMap = new HashMap<>();
 
@@ -23,6 +25,7 @@ public class FilmController {
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
     public ResponseEntity<Film> createOrUpdateFilm(@Valid @RequestBody Film film) {
         filmMap.put(film.getId(), film);
+        log.debug(String.format("Обновление/Создание объекта: %s", film.toString()));
         return ResponseEntity.ok(film);
     }
 
@@ -30,7 +33,8 @@ public class FilmController {
     public ResponseEntity<List<Error>> getError(MethodArgumentNotValidException ex) {
         List<Error> errors = new ArrayList<>();
         for (FieldError error : ex.getFieldErrors()) {
-            errors.add(new Error(error.getField(),error.getDefaultMessage()));
+            errors.add(new Error(error.getField(), error.getDefaultMessage()));
+            log.error(String.format("Ошибка при создании/Обновлении объекта: %s", error.getDefaultMessage()));
         }
         return ResponseEntity.badRequest().body(errors);
     }
--- src/main/java/application/controllers/UserController.java
+++ src/main/java/application/controllers/UserController.java
@@ -1,6 +1,7 @@
 package application.controllers;
 
 import application.model.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +13,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private final Map<Integer, User> userMap = new HashMap<>();
 
@@ -23,6 +25,7 @@ public class UserController {
     @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
     public User createOrUpdateUser(@Valid @RequestBody User user) {
         userMap.put(user.getId(), user);
+        log.debug(String.format("Обновление/Создание объекта: %s", user.toString()));
         return user;
     }
 
@@ -31,6 +34,7 @@ public class UserController {
         List<Error> errors = new ArrayList<>();
         for (FieldError error : ex.getFieldErrors()) {
             errors.add(new Error(error.getField(), error.getDefaultMessage()));
+            log.error(String.format("Ошибка при создании/Обновлении объекта: %s", error.getDefaultMessage()));
         }
         return ResponseEntity.badRequest().body(errors);
     }
