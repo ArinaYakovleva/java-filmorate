f3042d4bef3ffe41d947ac4e880a23155213026f
Arina <yakovlevaarischa@yandex.ru>
2022-08-04 01:02:20 +0300

    feat: constant data insertions

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.age_restrictions (restriction_name) VALUES ('G');
+INSERT INTO public.age_restrictions (restriction_name) VALUES ('PG');
+INSERT INTO public.age_restrictions (restriction_name) VALUES ('PG-13');
+INSERT INTO public.age_restrictions (restriction_name) VALUES ('R');
+INSERT INTO public.age_restrictions (restriction_name) VALUES ('NC-17');
+
+INSERT INTO public.genres (genre_name) VALUES ('Мелодрама');
+INSERT INTO public.genres (genre_name) VALUES ('Драма');
+INSERT INTO public.genres (genre_name) VALUES ('Комедия');
+INSERT INTO public.genres (genre_name) VALUES ('Мультфильм');
+INSERT INTO public.genres (genre_name) VALUES ('Триллер');
+INSERT INTO public.genres (genre_name) VALUES ('Документальный');
+INSERT INTO public.genres (genre_name) VALUES ('Боевик');
